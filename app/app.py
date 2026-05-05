5e20d0ff6f66be2f7e17743f302b98a5934bda3e
Nafees Anwer <your-email@example.com>
2026-05-05 13:09:13 +0000

    6th

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import socket
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from GKE Autopilot CI/CD 🚀"
+    return f"""
+    🚀 CI/CD SUCCESSFUL 🚀 <br><br>
+    Version: v2 <br>
+    Deployed at: {datetime.datetime.now()} <br>
+    Hostname: {socket.gethostname()} <br>
+    """
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
